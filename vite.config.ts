0172d37d1819378a18929bd7a8b8a322ea530b51
Sam Daramroei <samdotmc@gmail.com>
2025-06-07 00:50:04 +0300

    feat: add GitHub Actions workflow for deployment; update manifest and index.html for proper asset linking

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/elitexteriorsv2/", // Add this line - should match your repository name
   plugins: [tailwindcss(), react()],
 });
